8d740ed5726550b696a2b257f1bfb91d175351a5
sanotakumi <t.sano.19941118@gmail.com>
2021-11-06 05:30:46 +0000

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about' => 'homes#about', as: 'about'
   post '/users/:id' => 'users#create'
-  resources :users, only: [:index, :create, :show, :edit, :update]
+  resources :users, only: [:index, :create, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
